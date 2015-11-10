23c9c94ea0ec50bb3b9d1a512db0508e06a9421f
jkeim <jkeim@elevenfiftyacademy.org>
2015-11-10 10:09:06 -0500

    Refactored Produce bean to be in the proper package

--- src/org/elevenfifty/smoothie/beans/Produce.java
+++ src/org/elevenfifty/smoothie/beans/Produce.java
@@ -1,46 +1,51 @@
-
+package org.elevenfifty.smoothie.beans;
 
 public class Produce {
 
-	
 	private int calories;
 	private String name;
-	private String type; 
+	private String type;
 	private double price;
 	private int weight;
-	
+
 	public int getCalories() {
 		return calories;
 	}
+
 	public void setCalories(int calories) {
 		this.calories = calories;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public int getWeight() {
 		return weight;
 	}
+
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-	
-	
-}
 
-	
+}
