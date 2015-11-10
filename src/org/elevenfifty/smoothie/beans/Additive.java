d2596807e5e823a7b3c7a33bc0cfffbd14612e1a
Eric Kindler <ekindler@elevenfiftyconsulting.com>
2015-11-10 13:01:01 -0500

    added some possibly useful variables

--- src/org/elevenfifty/smoothie/beans/Additive.java
+++ src/org/elevenfifty/smoothie/beans/Additive.java
@@ -1,32 +1,62 @@
 package org.elevenfifty.smoothie.beans;
 
-
 public class Additive {
-	public static enum Category {PROTEIN, NUTRIENT, ENERGY, ANTIOXIDANT};
+	public static enum Category {
+		PROTEIN, NUTRIENT, ENERGY, ANTIOXIDANT
+	};
+
 	private String name;
 	private Category category;
 	private double price;
-	
+	private int weight;
+	private boolean organic = false;
+	private int calories;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	
-	
 
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public boolean isOrganic() {
+		return organic;
+	}
+
+	public void setOrganic(boolean organic) {
+		this.organic = organic;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
 }
