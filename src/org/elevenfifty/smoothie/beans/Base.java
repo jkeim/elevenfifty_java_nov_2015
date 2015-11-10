c001242656c69fd9e9a42cfecba1c6caf3ba7e73
Jeff Roberg <jroberg@nckwebtech.com>
2015-11-10 09:10:56 -0600

    Add Base

---
+++ src/org/elevenfifty/smoothie/beans/Base.java
@@ -0,0 +1,37 @@
+package org.elevenfifty.smoothie.beans;
+
+public class Base {
+	
+	private String name;
+	private int weight; // grams
+	private int calories;
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+
+}
